a6dbe103d65815b7b34b5510cddc201874d767f5
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2025-12-17 13:31:36 +0530

    implementing service and controller logic in user_service, also add aspect object model

--- user-service/src/main/java/com/rony/user_service/controller/UserController.java
+++ user-service/src/main/java/com/rony/user_service/controller/UserController.java
@@ -5,10 +5,7 @@ import com.rony.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -23,4 +20,32 @@ public class UserController {
         return new  ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id){
+        UserDto userDto = userService.getUserById(id);
+        if(userDto == null){
+          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody UserDto userDto){
+        try{
+            userService.updateUser(id, userDto);
+            return ResponseEntity.ok("User updated successfully");
+        } catch (IllegalArgumentException e) {
+          return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
