e8b69e8b6207a0752c478fd4ef2c539b4602257e
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2025-12-24 14:11:03 +0530

    feat: add usage-service for energy aggregation and alerts

---
+++ usage-service/src/main/java/com/rony/usage_service/dto/DeviceDto.java
@@ -0,0 +1,10 @@
+package com.rony.usage_service.dto;
+
+import lombok.Builder;
+
+@Builder
+public record DeviceDto(Long id,
+                        String name,
+                        String type,
+                        String location,
+                        Long userId) {}
