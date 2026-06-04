713867596a982199304b758dcec84dec987d3058
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2026-06-04 13:48:48 +0530

    feat: implement device service with user association and API testing

--- api-gateway/src/main/java/com/rony/api_gateway/route/UserServiceRoutes.java
+++ api-gateway/src/main/java/com/rony/api_gateway/route/UserServiceRoutes.java
@@ -1,9 +1,11 @@
 package com.rony.api_gateway.route;
 
 import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.function.HandlerFilterFunction;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -11,6 +13,7 @@ import org.springframework.web.servlet.function.ServerResponse;
 import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
@@ -37,4 +40,15 @@ public class UserServiceRoutes {
                                 .body("User service is down"))
                 .build();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceApiDocs() {
+        return GatewayRouterFunctions.route("user-service-api-docs")
+                .route(RequestPredicates.path("/docs/user-service/v3/api-docs"),
+                        http())
+                .before(uri("http://localhost:8080"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
+
 }
